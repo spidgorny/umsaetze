6b77d053ecea3400837849c939fdefbba9f8dd14
DEPIDSVY <depidsvy@nintendo.de>
2016-07-29 11:36:14 +0200

    - ADD Promises to be able to run categorize() after reading Excel file

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 /// <reference path="typings/index.d.ts" />
 var SpardaBank = (function () {
     function SpardaBank() {
+        this.sourceFile = 'umsaetze-1090729-2016-07-27-00-11-29.csv';
+        this.keywordFile = 'keywords.xlsx';
+        this.keyWords = {};
     }
     SpardaBank.prototype.convertMoneyFormat = function () {
         var fs = require('fs');
-        var parse = require('csv-parse');
         var transform = require('stream-transform');
-        var parser = parse({
-            delimiter: ';',
-            columns: true,
-            comment: '#',
-            skip_empty_lines: true
-        });
-        var sourceFile = 'umsaetze-1090729-2016-07-27-00-11-29.csv';
-        var input = fs.createReadStream(sourceFile);
         var storeColumns = null;
+        var rows = 0;
         var transformer = transform(function (record, callback) {
             var amount = record.amount;
             amount = amount.replace('.', '');
@@ -33,43 +28,101 @@ var SpardaBank = (function () {
                 }
                 console.log(storeColumns);
             }
+            rows++;
         }, { parallel: 1 });
-        var path = require('path');
-        var ext = path.extname(sourceFile);
-        var destination = path.basename(sourceFile, ext) + '.import' + ext;
-        console.log(destination);
-        var output = fs.createWriteStream(destination);
         var stringify = require('csv-stringify');
         var stringifier = stringify({
             header: true,
             columns: storeColumns,
             delimiter: ';'
         });
-        input
-            .pipe(parser)
+        var path = require('path');
+        var ext = path.extname(this.sourceFile);
+        var destination = path.basename(this.sourceFile, ext) + '.import' + ext;
+        console.log('Destination: ', destination);
+        var output = fs.createWriteStream(destination);
+        var input = fs.createReadStream(this.sourceFile);
+        var streamToPromise = require('stream-to-promise');
+        streamToPromise(input
+            .pipe(this.getParser())
             .pipe(transformer)
             .pipe(stringifier)
-            .pipe(output);
+            .pipe(output)).then(function () {
+            console.log('Rows processed: ', rows);
+        });
+    };
+    SpardaBank.prototype.getParser = function () {
+        var parse = require('csv-parse');
+        var parser = parse({
+            delimiter: ';',
+            columns: true,
+            comment: '#',
+            skip_empty_lines: true
+        });
+        return parser;
     };
     SpardaBank.prototype.readExcelFile = function () {
         var _this = this;
-        var Excel = require('exceljs');
-        var workbook = new Excel.Workbook();
-        workbook.xlsx.readFile('keywords.xlsx')
-            .then(function () {
-            var sheet = workbook.getWorksheet(1);
-            _this.dumpSheet(sheet);
+        return new Promise(function (resolve, reject) {
+            var Excel = require('exceljs');
+            var workbook = new Excel.Workbook();
+            workbook.xlsx.readFile(_this.keywordFile)
+                .then(function () {
+                var sheet = workbook.getWorksheet(1);
+                _this.dumpSheet(sheet);
+                resolve(true);
+            });
         });
     };
     SpardaBank.prototype.dumpSheet = function (sheet) {
-        sheet.eachRow(function (row, rowNumber) {
-            cells = row.values.slice(0, 2);
-            console.log(cells.join('\t'));
-            console.log(JSON.stringify(cells));
+        var _this = this;
+        return new Promise(function (resolve, reject) {
+            sheet.eachRow(function (row, rowNumber) {
+                var cells = row.values;
+                // not sure why this is needed
+                // var cells = row.values.slice(0, 2);
+                //console.log(cells.join('\t'));
+                //console.log(JSON.stringify(cells));
+                var key = cells[1];
+                var category = cells[2];
+                //console.log(key, category);
+                _this.keyWords[key] = category;
+            });
+            console.log(_this.keyWords);
+            resolve(true);
         });
+    };
+    SpardaBank.prototype.categorize = function () {
+        var fs = require('fs');
+        var transform = require('stream-transform');
+        var transformer = transform(function (record, callback) {
+            var shortNote = record.note.substr(0, 100);
+            console.log(SpardaBank.twoTabs(record.amount), '\t', record.category, '\t', shortNote);
+            callback(null, record);
+        }, { parallel: 1 });
+        var input = fs.createReadStream(this.sourceFile);
+        input
+            .pipe(this.getParser())
+            .pipe(transformer);
+    };
+    /**
+     * Makes sure text is shown on two tabs
+     * @param text
+     * @returns {string}
+     */
+    SpardaBank.twoTabs = function (text) {
+        if (text.length < 8) {
+            text += '\t';
+        }
+        return text;
     };
     return SpardaBank;
 }());
 var sb = new SpardaBank();
-sb.readExcelFile();
+sb.convertMoneyFormat();
+sb.readExcelFile().then(function (x) {
+    sb.categorize();
+}).catch(function (e) {
+    console.log('Promise error: ' + e);
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,23 +1,32 @@
 /// <reference path="typings/index.d.ts" />
 
+declare class Record {
+
+	account: string;
+	category: string;
+	currency: string;
+	amount: string;
+	payment_type: string;
+	date: string;
+	note: string;
+
+}
+
 class SpardaBank {
+
+	sourceFile = 'umsaetze-1090729-2016-07-27-00-11-29.csv';
+
+	keywordFile = 'keywords.xlsx';
+
+	keyWords = {};
 
 	convertMoneyFormat() {
 		var fs = require('fs');
-		var parse = require('csv-parse');
 		var transform = require('stream-transform');
 
-		var parser = parse({
-			delimiter: ';',
-			columns: true,
-			comment: '#',
-			skip_empty_lines: true,
-		});
-		var sourceFile = 'umsaetze-1090729-2016-07-27-00-11-29.csv';
-		var input = fs.createReadStream(sourceFile);
-
 		var storeColumns = null;
-		var transformer = transform(function (record, callback) {
+		var rows = 0;
+		var transformer = transform(function (record: Record, callback) {
 			var amount = record.amount;
 			amount = amount.replace('.', '');
 			amount = amount.replace(',', '.');
@@ -35,13 +44,8 @@ class SpardaBank {
 				}
 				console.log(storeColumns);
 			}
+			rows++;
 		}, {parallel: 1});
-
-		var path = require('path');
-		var ext = path.extname(sourceFile);
-		var destination = path.basename(sourceFile, ext) + '.import' + ext;
-		console.log(destination);
-		var output = fs.createWriteStream(destination);
 
 		var stringify = require('csv-stringify');
 		var stringifier = stringify({
@@ -50,34 +54,104 @@ class SpardaBank {
 			delimiter: ';',
 		});
 
-		input
-			.pipe(parser)
-			.pipe(transformer)
-			.pipe(stringifier)
-			// .pipe(process.stdout)
-			.pipe(output)
-		;
+		var path = require('path');
+		var ext = path.extname(this.sourceFile);
+		var destination = path.basename(this.sourceFile, ext) + '.import' + ext;
+		console.log('Destination: ', destination);
+		var output = fs.createWriteStream(destination);
+
+		var input = fs.createReadStream(this.sourceFile);
+		var streamToPromise = require('stream-to-promise');
+		streamToPromise(input
+		.pipe(this.getParser())
+		.pipe(transformer)
+		.pipe(stringifier)
+		// .pipe(process.stdout)
+		.pipe(output)).then(() => {
+			console.log('Rows processed: ', rows);
+		});
+	}
+
+	getParser() {
+		var parse = require('csv-parse');
+		var parser = parse({
+			delimiter: ';',
+			columns: true,
+			comment: '#',
+			skip_empty_lines: true,
+		});
+		return parser;
 	}
 
 	readExcelFile() {
-		var Excel = require('exceljs');
-		var workbook = new Excel.Workbook();
-		workbook.xlsx.readFile('keywords.xlsx')
-			.then(() => {
-				var sheet = workbook.getWorksheet(1);
-				this.dumpSheet(sheet);
-			});
+		return new Promise((resolve, reject) => {
+			var Excel = require('exceljs');
+			var workbook = new Excel.Workbook();
+			workbook.xlsx.readFile(this.keywordFile)
+				.then(() => {
+					var sheet = workbook.getWorksheet(1);
+					this.dumpSheet(sheet);
+					resolve(true);
+				});
+		});
 	}
 
 	dumpSheet(sheet) {
-		sheet.eachRow((row, rowNumber) => {
-			cells = row.values.slice(0, 2);
-			console.log(cells.join('\t'));
-			console.log(JSON.stringify(cells));
+		return new Promise((resolve, reject) => {
+			sheet.eachRow((row, rowNumber) => {
+				var cells = row.values;
+				// not sure why this is needed
+				// var cells = row.values.slice(0, 2);
+
+				//console.log(cells.join('\t'));
+				//console.log(JSON.stringify(cells));
+				var key = cells[1];
+				var category = cells[2];
+				//console.log(key, category);
+				this.keyWords[key] = category;
+			});
+			console.log(this.keyWords);
+			resolve(true);
 		});
+	}
+
+	categorize() {
+		var fs = require('fs');
+		var transform = require('stream-transform');
+
+		var transformer = transform((record: Record, callback) => {
+			var shortNote = record.note.substr(0, 100);
+			console.log(
+				SpardaBank.twoTabs(record.amount), '\t',
+				record.category, '\t',
+				shortNote);
+			callback(null, record);
+		}, {parallel: 1});
+
+		var input = fs.createReadStream(this.sourceFile);
+		input
+			.pipe(this.getParser())
+			.pipe(transformer);
+	}
+
+	/**
+	 * Makes sure text is shown on two tabs
+	 * @param text
+	 * @returns {string}
+	 */
+	static twoTabs(text: string) {
+		if (text.length < 8) {
+			text += '\t';
+		}
+		return text;
 	}
 
 }
 
 var sb = new SpardaBank();
-sb.readExcelFile();
+sb.convertMoneyFormat();
+sb.readExcelFile().then((x) => {
+	sb.categorize();
+}).catch((e) => {
+	console.log('Promise error: ' + e);
+});
