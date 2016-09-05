9ba9b8309d471dd3bd706733dd799d2de66c719f
DEPIDSVY <depidsvy@nintendo.de>
2016-09-05 19:17:11 +0200

    - ADD showing the totals for each category on the sidebar

--- web/umsaetze.js
+++ web/umsaetze.js
@@ -12,13 +12,26 @@ var Transaction = (function (_super) {
     }
     return Transaction;
 }(Backbone.Model));
+function asyncLoop(arr, callback, done) {
+    (function loop(i) {
+        callback(arr[i], i, arr.length); //callback when the loop goes on
+        if (i < arr.length) {
+            setTimeout(function () { loop(++i); }, 1); //rerun when condition is true
+        }
+        else {
+            if (done) {
+                done(arr.length); //callback when the loop ends
+            }
+        }
+    }(0)); //start with 0
+}
 var Expenses = (function (_super) {
     __extends(Expenses, _super);
     function Expenses() {
         _super.apply(this, arguments);
         this.attributes = null;
         this.model = Transaction;
-        this.csvUrl = '../umsaetze-1090729-2016-07-27-00-11-29.import.csv';
+        this.csvUrl = '../umsaetze-1090729-2016-07-27-00-11-29.cat.csv';
     }
     Expenses.prototype.fetch = function (options) {
         var _this = this;
@@ -30,15 +43,24 @@ var Expenses = (function (_super) {
                 skipEmptyLines: true
             });
             //console.log(csv);
-            _.map(csv.data, function (row) {
-                //console.log(row);
-                _this.add(new Transaction(row));
-            });
-            if (options.success) {
-                options.success.call();
-            }
-            _this.trigger('change');
+            _.each(csv.data, _this.processRow.bind(_this));
+            _this.processDone(csv.data.length, options);
+            // asyncLoop(csv.data, this.processRow.bind(this),	this.processDone.bind(this));
         });
+    };
+    Expenses.prototype.processRow = function (row, i, length) {
+        var percent = Math.round(100 * i / length);
+        //console.log(row);
+        $('.progress .progress-bar').width(percent + '%');
+        this.add(new Transaction(row));
+        //this.trigger('change');
+    };
+    Expenses.prototype.processDone = function (count, options) {
+        console.log('asyncLoop finished', count);
+        if (options.success) {
+            options.success.call();
+        }
+        this.trigger('change');
     };
     return Expenses;
 }(Backbone.Collection));
@@ -77,11 +99,77 @@ var ExpenseTable = (function (_super) {
     };
     return ExpenseTable;
 }(Backbone.View));
+var CategoryCount = (function () {
+    function CategoryCount() {
+    }
+    return CategoryCount;
+}());
+var CategoryView = (function (_super) {
+    __extends(CategoryView, _super);
+    function CategoryView(options) {
+        _super.call(this, options);
+        this.categoryCount = [];
+        this.template = _.template($('#categoryTemplate').html());
+        this.setElement($('#categories'));
+        this.categoryCount = [];
+    }
+    CategoryView.prototype.render = function () {
+        var _this = this;
+        var content = [];
+        var sum = _.reduce(this.categoryCount, function (memo, item) {
+            // only expenses
+            if (item.catName != 'Default' && item.amount < 0) {
+                return memo + item.amount;
+            }
+            else {
+                return memo;
+            }
+        }, 0);
+        console.log('sum', sum);
+        this.categoryCount = _.sortBy(this.categoryCount, function (el) {
+            return -el.amount;
+        }).reverse();
+        _.each(this.categoryCount, function (catCount) {
+            if (catCount.catName != 'Default' && catCount.amount < 0) {
+                var width = Math.round(100 * (-catCount.amount) / -sum) + '%';
+                console.log(catCount.catName, width, catCount.count, catCount.amount);
+                content.push(_this.template(_.extend(catCount, {
+                    width: width,
+                    amount: Math.round(catCount.amount)
+                })));
+            }
+        });
+        this.$el.html(content.join('\n'));
+        return this;
+    };
+    CategoryView.prototype.change = function () {
+        var _this = this;
+        console.log('model changed', this.model.size());
+        this.model.each(function (transaction) {
+            var categoryName = transaction.get('category');
+            var exists = _.findWhere(_this.categoryCount, { catName: categoryName });
+            if (exists) {
+                exists.count++;
+                exists.amount += parseFloat(transaction.get('amount'));
+            }
+            else {
+                _this.categoryCount.push({
+                    catName: categoryName,
+                    count: 0,
+                    amount: 0
+                });
+            }
+        });
+        console.log(this.categoryCount);
+        this.render();
+    };
+    return CategoryView;
+}(Backbone.View));
 var AppView = (function (_super) {
     __extends(AppView, _super);
-    function AppView() {
+    function AppView(options) {
         var _this = this;
-        this.table = null;
+        _super.call(this, options);
         console.log('construct AppView');
         this.setElement($('#app'));
         this.model = new Expenses();
@@ -89,13 +177,17 @@ var AppView = (function (_super) {
             model: this.model,
             el: $('#expenseTable')
         });
+        this.categories = new CategoryView({
+            model: this.model
+        });
         this.startLoading();
         this.model.fetch({
             success: function () {
                 _this.stopLoading();
             }
         });
-        this.listenTo(this.model, "change", this.render);
+        this.listenTo(this.model, "change", this.render.bind(this));
+        this.listenTo(this.model, "change", this.categories.change.bind(this.categories));
     }
     AppView.prototype.startLoading = function () {
         console.log('startLoading');
--- web/umsaetze.ts
+++ web/umsaetze.ts
@@ -5,10 +5,30 @@ import CollectionFetchOptions = Backbone.CollectionFetchOptions;
 
 class Transaction extends Backbone.Model {
 
+	date: Date;
+	category: String;
+	amount: Number;
+	note: String;
+
 	// constructor(row) {
 		//console.log('Transaction.initialize', row);
 	// }
 
+}
+
+function asyncLoop(arr: Array<any>, callback: Function, done?: Function) {
+	(function loop(i) {
+
+		callback(arr[i], i, arr.length);                            //callback when the loop goes on
+
+		if (i < arr.length) {                      //the condition
+			setTimeout(function() {loop(++i)}, 1); //rerun when condition is true
+		} else {
+			if (done) {
+				done(arr.length);                            //callback when the loop ends
+			}
+		}
+	}(0));                                         //start with 0
 }
 
 class Expenses extends Backbone.Collection<Transaction> {
@@ -17,7 +37,7 @@ class Expenses extends Backbone.Collection<Transaction> {
 
 	model = Transaction;
 
-	csvUrl = '../umsaetze-1090729-2016-07-27-00-11-29.import.csv';
+	csvUrl = '../umsaetze-1090729-2016-07-27-00-11-29.cat.csv';
 
 	fetch(options?: CollectionFetchOptions) {
 		console.log('csvUrl', this.csvUrl);
@@ -28,15 +48,26 @@ class Expenses extends Backbone.Collection<Transaction> {
 				skipEmptyLines: true
 			});
 			//console.log(csv);
-			_.map(csv.data, (row) => {
-				//console.log(row);
-				this.add(new Transaction(row));
-			});
-			if (options.success) {
-				options.success.call();
-			}
-			this.trigger('change');
+			_.each(csv.data, this.processRow.bind(this));
+			this.processDone(csv.data.length, options);
+			// asyncLoop(csv.data, this.processRow.bind(this),	this.processDone.bind(this));
 		});
+	}
+
+	processRow(row: Array, i: Number, length: Number) {
+		var percent = Math.round(100 * i / length);
+		//console.log(row);
+		$('.progress .progress-bar').width(percent+'%');
+		this.add(new Transaction(row));
+		//this.trigger('change');
+	}
+
+	processDone(count, options?: CollectionFetchOptions) {
+		console.log('asyncLoop finished', count);
+		if (options.success) {
+			options.success.call();
+		}
+		this.trigger('change');
 	}
 
 }
@@ -80,19 +111,100 @@ class ExpenseTable extends Backbone.View<Expenses> {
 
 }
 
+class CategoryCount {
+	catName: String;
+	count: Number;
+	amount: Number;
+}
+
+class CategoryView extends Backbone.View<Expenses> {
+
+	model: Expenses;
+
+	categoryCount = [];
+
+	template = _.template($('#categoryTemplate').html());
+
+	constructor(options) {
+		super(options);
+		this.setElement($('#categories'));
+		this.categoryCount = [];
+	}
+
+	render() {
+		var content = [];
+		var sum: Number = <Number>_.reduce(this.categoryCount,
+			(memo, item: CategoryCount) => {
+			// only expenses
+			if (item.catName != 'Default' && item.amount < 0) {
+				return memo + item.amount;
+			} else {
+				return memo;
+			}
+		}, 0);
+		console.log('sum', sum);
+
+		this.categoryCount = _.sortBy(this.categoryCount, (el: CategoryCount) => {
+			return -el.amount;
+		}).reverse();
+
+		_.each(this.categoryCount, (catCount: CategoryCount) => {
+			if (catCount.catName != 'Default' && catCount.amount < 0) {
+				var width = Math.round(100 * (-catCount.amount) / -sum) + '%';
+				console.log(catCount.catName, width, catCount.count, catCount.amount);
+				content.push(this.template(
+					_.extend(catCount, {
+						width: width,
+						amount: Math.round(catCount.amount),
+					})
+				));
+			}
+		});
+		this.$el.html(content.join('\n'));
+		return this;
+	}
+
+	change() {
+		console.log('model changed', this.model.size());
+		this.model.each((transaction: Transaction) => {
+			var categoryName = transaction.get('category');
+			var exists = _.findWhere(this.categoryCount, {catName: categoryName});
+			if (exists) {
+				exists.count++;
+				exists.amount += parseFloat(transaction.get('amount'));
+			} else {
+				this.categoryCount.push({
+					catName: categoryName,
+					count: 0,
+					amount: 0,
+				});
+			}
+		});
+		console.log(this.categoryCount);
+		this.render();
+	}
+
+}
+
 class AppView extends Backbone.View<Expenses> {
 
 	model: Expenses;
 
-	table = null;
+	table: ExpenseTable;
 
-	constructor() {
+	categories: CategoryView;
+
+	constructor(options) {
+		super(options);
 		console.log('construct AppView');
 		this.setElement($('#app'));
 		this.model = new Expenses();
 		this.table = new ExpenseTable({
 			model: this.model,
 			el: $('#expenseTable')
+		});
+		this.categories = new CategoryView({
+			model: this.model,
 		});
 
 		this.startLoading();
@@ -102,7 +214,8 @@ class AppView extends Backbone.View<Expenses> {
 			}
 		});
 
-		this.listenTo(this.model, "change", this.render);
+		this.listenTo(this.model, "change", this.render.bind(this));
+		this.listenTo(this.model, "change", this.categories.change.bind(this.categories));
 	}
 
 	startLoading() {
@@ -121,6 +234,7 @@ class AppView extends Backbone.View<Expenses> {
 		if (this.model && this.model.size()) {
 			this.table.render();
 			this.$el.html('Table shown');
+			//this.categories.render();
 		} else {
 			this.startLoading();
 		}
