aa95837d99b70a2756baf82c8d67f2f21ccdef3f
Slawa <depidsvy@nintendo.de>
2017-12-21 11:49:25 +0100

    - add routing working

--- v2/src/app/app.module.ts
+++ v2/src/app/app.module.ts
@@ -2,7 +2,6 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-
 import {AppComponent} from './app.component';
 import {ExpensesTableComponent} from './expenses-table/expenses-table.component';
 import {JsonDataSourceService} from './json-data-source.service';
@@ -12,15 +11,32 @@ import {ExpensesService} from './expenses.service';
 import {CategoryList} from './category-list';
 import { MonthSelectorComponent } from './month-selector/month-selector.component';
 import {CurrentMonthService} from './current-month.service';
+import { RouterModule, Routes } from '@angular/router';
+import {CategoryPageComponent} from './category-page/category-page.component';
+
+const appRoutes: Routes = [
+	{ path: 'categories', component: CategoryPageComponent},
+	{ path: 'dashboard', component: ExpensesTableComponent},
+	{ path: '',
+		redirectTo: '/dashboard',
+		pathMatch: 'full'
+	},
+	// { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
 	declarations: [
 		AppComponent,
 		ExpensesTableComponent,
 		CategorySelectorComponent,
-		MonthSelectorComponent
+		MonthSelectorComponent,
+		CategoryPageComponent
 	],
 	imports: [
+		RouterModule.forRoot(
+			appRoutes,
+			{ enableTracing: true } // <-- debugging purposes only
+		),
 		BrowserModule,
 		FormsModule,
 		HttpModule
