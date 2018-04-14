c7b805388b075c9fee0601e95207b8869e0e9001
Slawa Pidgorny <spidgorny@gmail.com>
2018-04-14 23:01:29 +0200

    - start fetchExpenses to link to the real bank

--- build/test/fetchExpenses.js
+++ build/test/fetchExpenses.js
@@ -6,24 +6,190 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const path = require('path');
+let configPath = path.resolve(__dirname, '../../.env');
+require('dotenv').config({
+    path: configPath,
+});
 const Swagger = require('swagger-client');
+const fs = require('fs');
+const log = require('ololog');
+class FetchExpenses {
+    constructor() {
+    }
+    initPets() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.client = yield Swagger('http://petstore.swagger.io/v2/swagger.json');
+        });
+    }
+    testPets() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.initPets();
+            this.client.apis.pet.addPet({ id: 1, name: "bobby" })
+                .then(ok => {
+                console.log(ok);
+            });
+            this.client.execute({
+                operationId: 'addPet', parameters: {
+                    id: 1,
+                    name: "bobby"
+                }
+            }).then(ok => {
+                log.error(ok);
+            });
+        });
+    }
+    initFinApi() {
+        return __awaiter(this, void 0, void 0, function* () {
+            this.client = yield Swagger({
+                url: 'https://docs.finapi.io/js/swagger.json',
+                requestInterceptor: (req) => {
+                    if (this.token && 'access_token' in this.token) {
+                        req.headers['Authorization'] = 'Bearer ' + this.token.access_token;
+                    }
+                    req.headers['Content-Type'] = 'application/json';
+                    return req;
+                }
+            });
+        });
+    }
+    testFinApi() {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield this.initFinApi();
+            let UserUsername = process.env['UserUsername'];
+            let UserPassword = process.env['UserPassword'];
+            log('Auth...', UserUsername, UserPassword);
+            yield this.loginUser(UserUsername, UserPassword);
+            let UserBLZ = process.env['UserBLZ'];
+            let UserKonto = process.env['UserKonto'];
+            let UserPIN = process.env['UserPIN'];
+            log('Connection...');
+            let connection = yield this.getConnection(UserBLZ);
+            if (!connection) {
+                log('Bank...', UserBLZ);
+                const bank = yield this.findBank(UserBLZ);
+                log('MakeConnection...');
+                connection = yield this.makeConnection(bank, UserKonto, UserPIN);
+            }
+            log('Accounts...');
+            let account = yield this.getAccounts(connection, UserKonto);
+            log(account);
+            let transactions = yield this.getTransactions(account);
+            log(transactions.length);
+        });
+    }
+    login() {
+        return __awaiter(this, void 0, void 0, function* () {
+            const tokenFile = path.resolve(__dirname, 'token.json');
+            if (fs.existsSync(tokenFile)) {
+                const data = fs.readFileSync(tokenFile);
+                this.token = JSON.parse(data);
+            }
+            else {
+                if (!process.env['ClientID']) {
+                    throw new Error('Provide ClientID env');
+                }
+                if (!process.env['Secret']) {
+                    throw new Error('Provide Secret env');
+                }
+                const token = yield this.client.apis.Authorization.getToken({
+                    grant_type: 'client_credentials',
+                    client_id: process.env['ClientID'],
+                    client_secret: process.env['Secret'],
+                });
+                this.token = token.body;
+                fs.writeFileSync(tokenFile, JSON.stringify(this.token));
+            }
+        });
+    }
+    findBank(blz) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.client.apis.Banks.getAndSearchAllBanks({
+                search: blz,
+            });
+            if (results.body.banks.length == 1) {
+                return results.body.banks[0];
+            }
+            else {
+                throw new Error(results.body.banks.length + ' banks found. Should be one.');
+            }
+        });
+    }
+    loginUser(username, password) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const token = yield this.client.apis.Authorization.getToken({
+                grant_type: 'password',
+                client_id: process.env['ClientID'],
+                client_secret: process.env['Secret'],
+                username,
+                password,
+            });
+            this.token = token.body;
+        });
+    }
+    getConnection(blz) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.client.apis['Bank Connections'].getAllBankConnections();
+            for (let c of results.body.connections) {
+                if (c.bank.blz == blz) {
+                    return c;
+                }
+            }
+            return null;
+        });
+    }
+    makeConnection(bank, account, password) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.client.apis['Bank Connections'].importBankConnection({
+                body: JSON.stringify({
+                    bankId: bank.id,
+                    bankingUserId: account,
+                    bankingPin: password,
+                })
+            });
+            log('makeConnection', results.body);
+            return results.body;
+        });
+    }
+    getAccounts(connection, account) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.client.apis.Accounts.getAndSearchAllAccounts({
+                bankConnectionIds: [connection.id],
+            });
+            for (let c of results.body.accounts) {
+                if (c.accountNumber == account) {
+                    return c;
+                }
+            }
+            return null;
+        });
+    }
+    getTransactions(account) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const collection = [];
+            let results = yield this.client.apis.Transactions.getAndSearchAllTransactions({
+                accountIds: [account.id],
+                view: 'bankView',
+            });
+            collection.push(...results.body.transactions);
+            log(results.body.paging);
+            while (results.body.paging.page < results.body.paging.pageCount) {
+                log('page', results.body.paging.page, 'of', results.body.paging.pageCount, collection.length);
+                results = yield this.client.apis.Transactions.getAndSearchAllTransactions({
+                    accountIds: [account.id],
+                    view: 'bankView',
+                    page: results.body.paging.page + 1,
+                });
+                collection.push(...results.body.transactions);
+            }
+            return collection;
+        });
+    }
+}
 (() => __awaiter(this, void 0, void 0, function* () {
     try {
-        const client = yield Swagger('http://petstore.swagger.io/v2/swagger.json');
-        console.log(client.spec);
-        console.log(client.originalSpec);
-        console.log(client.errors);
-        client.apis.pet.addPet({ id: 1, name: "bobby" }).then(ok => {
-            console.log(ok);
-        });
-        client.execute({
-            operationId: 'addPet', parameters: {
-                id: 1,
-                name: "bobby"
-            }
-        }).then(ok => {
-            console.log(ok);
-        });
+        const fe = new FetchExpenses();
+        yield fe.testFinApi();
     }
     catch (e) {
         console.error(e);
--- src/test/fetchExpenses.ts
+++ src/test/fetchExpenses.ts
@@ -1,28 +1,209 @@
+const path = require('path');
+let configPath = path.resolve(__dirname, '../../.env');
+// console.log(configPath);
+require('dotenv').config({
+	path: configPath,
+});
+// console.dir(process.env);
 // import Swagger from 'swagger-client';
 const Swagger = require('swagger-client');
+const fs = require('fs');
+const log = require('ololog');
 
-// https://docs.finapi.io/js/swagger.json
-(async () => {
-	try {
-		const client = await Swagger('http://petstore.swagger.io/v2/swagger.json');
-		console.log(client.spec); // The resolved spec
-		console.log(client.originalSpec); // In case you need it
-		console.log(client.errors); // Any resolver errors
+class FetchExpenses {
 
+	client: any/*Swagger*/;
+	token: {
+		access_token: string,
+		token_type: string,
+		expires_in: number,
+		scope: string,
+	};
+
+	constructor() {
+	}
+
+	async initPets() {
+		this.client = await Swagger('http://petstore.swagger.io/v2/swagger.json');
+		// console.log(this.client.spec); // The resolved spec
+		// console.log(this.client.originalSpec); // In case you need it
+		// console.log(this.client.errors); // Any resolver errors
+	}
+
+	async testPets() {
+		await this.initPets();
 		// Tags interface
-		client.apis.pet.addPet({id: 1, name: "bobby"}).then(ok => {
-			console.log(ok);
-		});
+		this.client.apis.pet.addPet({id: 1, name: "bobby"})
+			.then(ok => {
+				console.log(ok);
+			});
 
 		// TryItOut Executor, with the `spec` already provided
-		client.execute({
+		this.client.execute({
 			operationId: 'addPet', parameters: {
 				id: 1,
 				name: "bobby"
 			}
 		}).then(ok => {
-			console.log(ok);
+			log.error(ok);
 		});
+	}
+
+	async initFinApi() {
+		this.client = await Swagger({
+			url: 'https://docs.finapi.io/js/swagger.json',
+			requestInterceptor: (req) => {
+				//console.log(this.token);
+				if (this.token && 'access_token' in this.token) {
+					req.headers['Authorization'] = 'Bearer ' + this.token.access_token;
+				}
+				req.headers['Content-Type'] = 'application/json';
+				// log(req.headers);
+				return req;
+			}
+		});
+	}
+
+	async testFinApi() {
+		await this.initFinApi();
+		// await this.login();
+		let UserUsername = process.env['UserUsername'];
+		let UserPassword = process.env['UserPassword'];
+		log('Auth...', UserUsername, UserPassword);
+		await this.loginUser(UserUsername, UserPassword);
+
+		let UserBLZ = process.env['UserBLZ'];
+		let UserKonto = process.env['UserKonto'];
+		let UserPIN = process.env['UserPIN'];
+
+		log('Connection...');
+		let connection = await this.getConnection(UserBLZ);
+		if (!connection) {
+			log('Bank...', UserBLZ);
+			const bank = await this.findBank(UserBLZ);
+			log('MakeConnection...');
+			connection = await this.makeConnection(bank, UserKonto, UserPIN);
+		}
+
+		log('Accounts...');
+		let account = await this.getAccounts(connection, UserKonto);
+		log(account);
+
+		let transactions = await this.getTransactions(account);
+		log(transactions.length);
+	}
+
+	async login() {
+		const tokenFile = path.resolve(__dirname, 'token.json');
+		if (fs.existsSync(tokenFile)) {
+			const data = fs.readFileSync(tokenFile);
+			this.token = JSON.parse(data);
+		} else {
+			if (!process.env['ClientID']) {
+				throw new Error('Provide ClientID env');
+			}
+			if (!process.env['Secret']) {
+				throw new Error('Provide Secret env');
+			}
+			// console.log(process.env['ClientID'], process.env['Secret']);
+			const token = await this.client.apis.Authorization.getToken({
+				grant_type: 'client_credentials',
+				client_id: process.env['ClientID'],
+				client_secret: process.env['Secret'],
+			});
+			// console.log(token);
+			this.token = token.body;
+			fs.writeFileSync(tokenFile, JSON.stringify(this.token));
+		}
+	}
+
+	async findBank(blz) {
+		const results = await this.client.apis.Banks.getAndSearchAllBanks({
+			search: blz,
+		});
+		if (results.body.banks.length == 1) {
+			return results.body.banks[0];
+		} else {
+			throw new Error(results.body.banks.length + ' banks found. Should be one.')
+		}
+	}
+
+	async loginUser(username, password) {
+		const token = await this.client.apis.Authorization.getToken({
+			grant_type: 'password',
+			client_id: process.env['ClientID'],
+			client_secret: process.env['Secret'],
+			username,
+			password,
+		});
+		// console.log(token);
+		this.token = token.body;
+	}
+
+	async getConnection(blz) {
+		const results = await this.client.apis['Bank Connections'].getAllBankConnections();
+		// log('connections', results.body.connections);
+		for (let c of results.body.connections) {
+			if (c.bank.blz == blz) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	async makeConnection(bank, account, password) {
+		const results = await this.client.apis['Bank Connections'].importBankConnection({
+			body: JSON.stringify({
+				bankId: bank.id,
+				bankingUserId: account,
+				bankingPin: password,
+			})
+		});
+		log('makeConnection', results.body);
+		return results.body;
+	}
+
+	async getAccounts(connection, account) {
+		const results = await this.client.apis.Accounts.getAndSearchAllAccounts({
+			bankConnectionIds: [connection.id],
+		});
+		// log('connections', results.body.accounts);
+		for (let c of results.body.accounts) {
+			if (c.accountNumber == account) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	async getTransactions(account) {
+		const collection = [];
+		let results = await this.client.apis.Transactions.getAndSearchAllTransactions({
+			accountIds: [account.id],
+			view: 'bankView',
+		});
+		collection.push(...results.body.transactions);
+		log(results.body.paging);
+		while (results.body.paging.page < results.body.paging.pageCount) {
+			log('page', results.body.paging.page, 'of', results.body.paging.pageCount, collection.length);
+			results = await this.client.apis.Transactions.getAndSearchAllTransactions({
+				accountIds: [account.id],
+				view: 'bankView',
+				page: results.body.paging.page + 1,
+			});
+			collection.push(...results.body.transactions);
+		}
+		// log(results);
+		return collection;
+	}
+
+}
+
+(async () => {
+	try {
+		const fe = new FetchExpenses();
+		// await fe.testPets();
+		await fe.testFinApi();
 	} catch (e) {
 		console.error(e);
 	}
